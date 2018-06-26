0b78d6446f5605bcfba2a0c40222094b1c7eddaf
techtide <ab6.host@gmali.com>
2018-06-26 16:19:51 +0100

    initial commit of smart contract

---
+++ contracts/Sale.sol
@@ -0,0 +1,46 @@
+pragma solidity ^0.4.17;
+
+contract Sale {
+    enum DealType { physical, digital } 
+    enum PaymentOption { cash, crypto }
+    
+    struct Listing {
+        // General meta-data of the listing.
+        string listingName;
+        uint price; 
+        address listingMaker;
+        // Logistics of the listing: How will it happen? How will the buyer pay?
+        //DealType dealType;
+        //PaymentOption paymentOption;
+    }
+    
+    uint public currentKey;    
+    mapping (uint => Listing) public listings;
+
+    event ListingCreated(string nName, uint nPrice, address nListingMaker);
+
+    // Empty container for new listings [TO-DO: FIGURE OUT BETTER, MORE EFFICENT WAY SOON]
+    Listing private newListing; 
+
+    constructor () public {
+        currentKey = 0;    
+    }
+
+    function payForListing(uint listingID) public payable {
+        // Listing A: Car, 1 Ether, User A (address); with 100ETH
+                
+        // User B: Address; with 100ETH => payForListing
+        
+        // DEDUCT 1ETH FROM USER B => SALE.SOL (ADDRESS) TAKES 0.25(1ETH) => USER A GETS 1ETH-(0.25*1ETH)
+
+    }
+
+    function create(string nName, uint nPrice, address nListingMaker) public {
+        newListing = Listing({listingName: nName, price: nPrice, listingMaker: nListingMaker});
+        listings[currentKey+1]=newListing;
+    }
+
+    function deleteListing(uint id) public {
+        delete listings[id];
+    }
+}
---
+++ migrations/2_deploy_contract.js
@@ -0,0 +1,6 @@
+var Sale = artifacts.require("./Sale.sol");
+
+module.exports = function(deployer) {
+  deployer.deploy(Sale);
+};
+
